28beda71a601431e5e733bbf6a8f3f34587b5737
Bruno Hauck <brunohauck@Brunos-MacBook-Air.local>
2023-02-06 21:57:23 -0300

    Add Word Doc with images

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -36,5 +36,205 @@
 
 <?php wp_footer(); ?>
 
+<script type="text/javascript">
+
+	clevertap.event.push("Main Page Visited", {
+		"Name": "Main Page Wordpress",
+		"Date": new Date()
+	});			
+
+	
+	clevertap.onUserLogin.push({
+		"Site": {
+			"Name": "Bruno Hauck - Wordpress",            // String
+			"Identity": 619085435,              // String or number
+			"Email": "bruno@clevertap.com",         // Email address of the user
+			"Phone": "+14155551234",           // Phone (with the country code)
+			"Gender": "M",                     // Can be either M or F
+			"DOB": new Date(),                 // Date of Birth. Date object
+			// optional fields. controls whether the user will be sent email, push etc.
+			"MSG-email": false,                // Disable email notifications
+			"MSG-push": true,                  // Enable push notifications
+			"MSG-sms": true,                   // Enable sms notifications
+			"MSG-whatsapp": true,              // Enable WhatsApp notifications
+			"Team": "San Francisco 49",
+			"Score-value": 36, //integer value
+			"Spent": 55.22,
+			"Premium-account": true,
+			"Register-date": new Date(),
+		}
+	})
+
+	// Profile push used when you want do edit some info on the user
+
+	clevertap.profile.push({
+		"Site": {
+			"Name": "Bruno Hauck - Wordpress",            // String
+			"Identity": 619085435,              // String or number
+			"Email": "bruno@clevertap.com",         // Email address of the user
+			"Team": "San Francisco 49",
+			"Score-value": 36, //integer value
+			"Spent": 55.22,
+			"Premium-account": true,
+			"Register-date": new Date(),
+		}
+	})
+	
+	//Add Product - If you are using Woocomerce you need to install a plugin to add to the product this javascript
+	clevertap.event.push("Product Viewed", {
+		"Product name": "Casio Chronograph Watch",
+		"Category": "Mens Accessories",
+		"Price": 59.99,
+		"Date": new Date()
+	});
+
+	//clevertap.event.push("Product Viewed")
+	/*
+	clevertap.event.push("Product Viewed Web - CT Poc", {
+		"Product name": "Casio Chronograph Watch",
+		"Category": "Mens Accessories",
+		"Price": 59.99,
+	});*/
+
+
+	clevertap.event.push("Charged", {
+    "Amount": 300,
+    "Payment mode": "Credit Card",
+    "Charged ID": 24052013,
+    "Items": [
+        {
+            "Category": "Books",
+            "Book name": "The Millionaire next door",
+            "Quantity": 1
+        },
+        {
+            "Category": "Books",
+            "Book name": "Achieving inner zen",
+            "Quantity": 1
+        },
+        {
+            "Category": "Books",
+            "Book name": "Chuck it, let's do it",
+            "Quantity": 5
+        }
+    ]
+});
+
+
+
+</script>
+
+<div class="btn">CLOSE</div>
+<script>
+
+
+	//clevertap.setLogLevel(LOG_LEVEL)
+
+
+	clevertap.notificationCallback = function (msg) {
+		//raise the notification viewed and clicked events in the callback
+		/*
+		clevertap.raiseNotificationViewed();
+		console.log(JSON.stringify(msg));            //your custom rendering implementation here
+		var $button = jQuery("<button></button>");   //element on whose click you want to raise the notification clicked event
+		$button.click(function () {
+			clevertap.raiseNotificationClicked();
+		});*/
+	};
+
+	clevertap.notifications.push({
+		"apnsWebPushId": "<apple web push id>", //only for safari browser
+		"apnsWebPushServiceUrl": "<safari package service url>",//only for safari browser
+		"titleText": 'Would you like to receive Push Notifications?',
+		"bodyText": 'We promise to only send you relevant content and give you updates on your transactions',
+		"okButtonText": 'Sign me up!',
+		"rejectButtonText": 'No thanks',
+		"okButtonColor": '#f28046'
+	});
+
+
+	//Implementation of banner and others 
+
+	console.log('popup')
+
+
+	var btn = document.querySelector('.btn');
+	var wrapper = window.parent.document.getElementById('wizParDiv0');
+	btn.addEventListener('click', closePopUp);
+	function closePopUp() { wrapper.remove(); }
+
+	var btn = document.querySelector('.btn');
+	var wrapper = window.parent.document.getElementById('wizParDiv2');
+	btn.addEventListener('click', closePopUp);
+	function closePopUp() { wrapper.remove(); }
+
+	var btn = document.querySelector('.btn');
+	var overlay = window.parent.document.getElementById('intentOpacityDiv');
+	var wrapper = window.parent.document.getElementById('intentPreview');
+	btn.addEventListener('click', closePopUp);
+	function closePopUp() { overlay.remove(); wrapper.remove(); }
+
+
+
+	/*
+	window.addEventListener('DOMContentLoaded', event => {
+
+		// Navbar shrink function
+		var navbarShrink = function () {
+			const navbarCollapsible = document.body.querySelector('#mainNav');
+			if (!navbarCollapsible) {
+				return;
+			}
+			if (window.scrollY === 0) {
+				navbarCollapsible.classList.remove('navbar-shrink')
+			} else {
+				navbarCollapsible.classList.add('navbar-shrink')
+			}
+		};
+
+		clevertap.event.push("Product Viewed Web - CT Poc", {
+			"Product name": "Casio Chronograph Watch",
+			"Category": "Mens Accessories",
+			"Price": 59.99,
+		});
+
+
+
+		document.addEventListener('scroll', navbarShrink);
+
+
+		const mainNav = document.body.querySelector('#mainNav');
+		if (mainNav) {
+			new bootstrap.ScrollSpy(document.body, {
+				target: '#mainNav',
+				offset: 74,
+			});
+		};
+
+		// Collapse responsive navbar when toggler is visible
+		const navbarToggler = document.body.querySelector('.navbar-toggler');
+		const responsiveNavItems = [].slice.call(
+			document.querySelectorAll('#navbarResponsive .nav-link')
+		);
+		responsiveNavItems.map(function (responsiveNavItem) {
+			responsiveNavItem.addEventListener('click', () => {
+				if (window.getComputedStyle(navbarToggler).display !== 'none') {
+					navbarToggler.click();
+				}
+			});
+		});
+
+	}); */
+
+	//clevertap.event.push("Product Viewed Web - CT Poc")
+
+	clevertap.event.push("Product Viewed Web - CT Poc", {
+		"Product name": "Casio Chronograph Watch",
+		"Category": "Mens Accessories",
+		"Price": 59.99,
+	});
+
+	</script>
+
 </body>
 </html>
--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -7,61 +7,108 @@
  * @package storefront
  */
 
-?><!doctype html>
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-<?php wp_head(); ?>
-</head>
+	<?php wp_head(); ?>
+	<script type="text/javascript">
+		var clevertap = { event: [], profile: [], account: [], region: 'eu1', onUserLogin: [], notifications: [], privacy: [] };
+		// replace with the CLEVERTAP_ACCOUNT_ID with the actual ACCOUNT ID value from your Dashboard -> Settings page
+		clevertap.account.push({ "id": "R97-98R-W46Z" });//
+		clevertap.privacy.push({ optOut: false }); //set the flag to true, if the user of the device opts out of sharing their data
+		clevertap.privacy.push({ useIP: true }); //set the flag to true, if the user agrees to share their IP data
+		(function () {
+			var wzrk = document.createElement('script');
+			wzrk.type = 'text/javascript';
+			wzrk.async = true;
+			wzrk.src = ('https:' == document.location.protocol ? 'https://d2r1yp2w7bby2u.cloudfront.net' : 'http://static.clevertap.com') + '/js/clevertap.min.js';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(wzrk, s);
+		})();
 
-<body <?php body_class(); ?>>
+	</script>
 
-<?php wp_body_open(); ?>
 
-<?php do_action( 'storefront_before_site' ); ?>
 
-<div id="page" class="hfeed site">
-	<?php do_action( 'storefront_before_header' ); ?>
 
-	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 
-		<?php
-		/**
-		 * Functions hooked into storefront_header action
-		 *
-		 * @hooked storefront_header_container                 - 0
-		 * @hooked storefront_skip_links                       - 5
-		 * @hooked storefront_social_icons                     - 10
-		 * @hooked storefront_site_branding                    - 20
-		 * @hooked storefront_secondary_navigation             - 30
-		 * @hooked storefront_product_search                   - 40
-		 * @hooked storefront_header_container_close           - 41
-		 * @hooked storefront_primary_navigation_wrapper       - 42
-		 * @hooked storefront_primary_navigation               - 50
-		 * @hooked storefront_header_cart                      - 60
-		 * @hooked storefront_primary_navigation_wrapper_close - 68
-		 */
-		do_action( 'storefront_header' );
-		?>
+	<script type="module">
 
-	</header><!-- #masthead -->
+		// Initialize Firebase 
+		// Import the functions you need from the SDKs you need
+		import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+		import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+		// TODO: Add SDKs for Firebase products that you want to use
+		// https://firebase.google.com/docs/web/setup#available-libraries
 
-	<?php
-	/**
-	 * Functions hooked in to storefront_before_content
-	 *
-	 * @hooked storefront_header_widget_region - 10
-	 * @hooked woocommerce_breadcrumb - 10
-	 */
-	do_action( 'storefront_before_content' );
-	?>
+		// Your web app's Firebase configuration
+		// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+		const firebaseConfig = {
+			apiKey: "AIzaSyATexGjCe0zenntk4AXB8pp8z83Zilh7o4",
+			authDomain: "teste-clevertap.firebaseapp.com",
+			projectId: "teste-clevertap",
+			storageBucket: "teste-clevertap.appspot.com",
+			messagingSenderId: "600111574066",
+			appId: "1:600111574066:web:2ffaead57d1a91327de02b"
+		};
 
-	<div id="content" class="site-content" tabindex="-1">
-		<div class="col-full">
+		// Initialize Firebase
+		const app = initializeApp(firebaseConfig);
+		const analytics = getAnalytics(app);
 
-		<?php
-		do_action( 'storefront_content_top' );
+
+
+		/*
+		  // Import the functions you need from the SDKs you need
+		  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+		  // TODO: Add SDKs for Firebase products that you want to use
+		  // https://firebase.google.com/docs/web/setup#available-libraries
+  
+		  // Your web app's Firebase configuration
+		  const firebaseConfig = {
+			  apiKey: "AIzaSyATexGjCe0zenntk4AXB8pp8z83Zilh7o4",
+			  authDomain: "teste-clevertap.firebaseapp.com",
+			  projectId: "teste-clevertap",
+			  storageBucket: "teste-clevertap.appspot.com",
+			  messagingSenderId: "600111574066",
+			  appId: "1:600111574066:web:2ffaead57d1a91327de02b"
+		  };
+		  // Initialize Firebase
+		  const app = initializeApp(firebaseConfig);*/
+	</script>
+
+	<style>
+		</head><body <?php body_class(); ?>><?php wp_body_open(); ?> <?php do_action('storefront_before_site'); ?> <div id="page" class="hfeed site"><?php do_action('storefront_before_header'); ?><header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>"><?php
+						/**
+						 * Functions hooked into storefront_header action
+						 *
+						 * @hooked storefront_header_container                 - 0
+						 * @hooked storefront_skip_links                       - 5
+						 * @hooked storefront_social_icons                     - 10
+						 * @hooked storefront_site_branding                    - 20
+						 * @hooked storefront_secondary_navigation             - 30
+						 * @hooked storefront_product_search                   - 40
+						 * @hooked storefront_header_container_close           - 41
+						 * @hooked storefront_primary_navigation_wrapper       - 42
+						 * @hooked storefront_primary_navigation               - 50
+						 * @hooked storefront_header_cart                      - 60
+						 * @hooked storefront_primary_navigation_wrapper_close - 68
+						 */
+						do_action('storefront_header');
+						?> </header>< !-- #masthead --><?php
+						 /**
+						  * Functions hooked in to storefront_before_content
+						  *
+						  * @hooked storefront_header_widget_region - 10
+						  * @hooked woocommerce_breadcrumb - 10
+						  */
+						 do_action('storefront_before_content');
+						 ?><div id="content" class="site-content" tabindex="-1"><div class="col-full"><?php
+						 do_action('storefront_content_top');
